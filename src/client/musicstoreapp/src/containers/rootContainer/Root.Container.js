9a01c09bf1a227603265553951b7cdf29aa69c18
Beshoy Hindy <beshoy.hindy@gmail.com>
2017-09-18 03:10:51 +0200

    init app
    init the client app

---
+++ src/client/musicstoreapp/src/containers/rootContainer/Root.Container.js
@@ -0,0 +1,5 @@
+import Root from '../../components/rootComponent/Root.Component';
+
+const RootContainer = () => <Root />;
+
+export default RootContainer;
--- src/client/musicstoreapp/src/index.js
+++ src/client/musicstoreapp/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '../styles/index.css';
+// import RootContainer from './containers/RootContainer/Root.Container';
+import Root from './components/rootComponent/Root.Component';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// ReactDOM.render(<RootContainer />, document.getElementById('root'));
+// ReactDOM.render(<Root />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
